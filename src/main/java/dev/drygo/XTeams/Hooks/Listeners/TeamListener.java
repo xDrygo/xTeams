cd2b9cd90d3feb5a0a5129bca3077bf3d5a50f75
xDrygo <xdrygo-personal@outlook.com>
2025-07-20 17:26:05 -0700

    🔧 Added join and leave listeners for LuckPerms and Minecraft Team sync

---
+++ src/main/java/dev/drygo/XTeams/Hooks/Listeners/TeamListener.java
@@ -0,0 +1,41 @@
+package dev.drygo.XTeams.Hooks.Listeners;
+
+import dev.drygo.XTeams.API.Events.TeamJoinEvent;
+import dev.drygo.XTeams.API.Events.TeamLeaveEvent;
+import dev.drygo.XTeams.Models.Team;
+import dev.drygo.XTeams.XTeams;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class TeamListener implements Listener {
+    private final XTeams plugin;
+
+    public TeamListener(XTeams plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onTeamJoin(TeamJoinEvent event) {
+        Player player = (Player) event.getPlayer();
+        Team team = plugin.getTeamManager().getTeam(event.getTeamName());
+        if (plugin.isEnabledLuckPermsHook()) {
+            plugin.getLuckPermsGroupManager().applyGroup(player, team.getName());
+        }
+        if (plugin.isEnabledMinecraftTeamHook()) {
+            plugin.getMinecraftTeamManager().applyGroup(player, team.getName());
+        }
+    }
+
+    @EventHandler
+    public void onTeamLeave(TeamLeaveEvent event) {
+        Player player = (Player) event.getPlayer();
+        Team team = plugin.getTeamManager().getTeam(event.getTeamName());
+        if (plugin.isEnabledLuckPermsHook()) {
+            plugin.getLuckPermsGroupManager().removeGroup(player, team.getName());
+        }
+        if (plugin.isEnabledMinecraftTeamHook()) {
+            plugin.getMinecraftTeamManager().removeGroup(player, team.getName());
+        }
+    }
+}
