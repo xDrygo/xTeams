f54e2d9890ec61d5ac8caac79c89b23b8fb6eca2
xDrygo <xdrygo-personal@outlook.com>
2025-07-20 17:28:56 -0700

    🔧 Moved plugin enabling methods to LoadUtils and adding enabled hooks instances

---
+++ src/main/java/dev/drygo/XTeams/Utils/LoadUtils.java
@@ -0,0 +1,73 @@
+package dev.drygo.XTeams.Utils;
+
+import dev.drygo.XTeams.API.XTeamsAPI;
+import dev.drygo.XTeams.Commands.XTeamsCommand;
+import dev.drygo.XTeams.Commands.XTeamsTabCompleter;
+import dev.drygo.XTeams.Hooks.Listeners.TeamListener;
+import dev.drygo.XTeams.Hooks.PlaceholderAPI.XTeamsExpansion;
+import dev.drygo.XTeams.Managers.ConfigManager;
+import dev.drygo.XTeams.XTeams;
+import org.bukkit.Bukkit;
+
+import java.util.Objects;
+
+public class LoadUtils {
+    private final XTeams plugin;
+    private final ConfigManager configManager;
+
+    public LoadUtils(XTeams plugin, ConfigManager configManager) {
+        this.plugin = plugin;
+        this.configManager = configManager;
+    }
+
+    public void loadFeatures() {
+        XTeamsAPI.init(plugin.getTeamManager());
+        loadFiles();
+        loadCommand();
+        loadListeners();
+        loadPlaceholderAPI();
+        loadHooks();
+    }
+
+    public void loadFiles() {
+        configManager.loadConfig();
+        configManager.loadMessages();
+        configManager.loadTeamsFromConfig();
+    }
+
+    private void loadListeners() {
+        plugin.getServer().getPluginManager().registerEvents(new TeamListener(plugin), plugin);
+    }
+
+    private void loadCommand() {
+        if (plugin.getCommand("xteams") == null) {
+            plugin.getLogger().severe("❌ Error: xTeams command is not registered in plugin.yml");
+        } else {
+            Objects.requireNonNull(plugin.getCommand("xteams")).setExecutor(new XTeamsCommand(plugin));
+            Objects.requireNonNull(plugin.getCommand("xteams")).setTabCompleter(new XTeamsTabCompleter(plugin));
+        }
+    }
+    private void loadPlaceholderAPI() {
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new XTeamsExpansion(plugin).register();
+            plugin.getLogger().info("✅ PlaceholderAPI detected. Placeholders will work.");
+            plugin.setWorkingPlaceholderAPI(true);
+        } else {
+            plugin.getLogger().warning("⚠ PlaceholderAPI not detected. Placeholders will not work.");
+        }
+    }
+    private void loadHooks() {
+        if (plugin.getConfig().getBoolean("hooks.luckperms.enabled", false)) {
+            if (Bukkit.getPluginManager().getPlugin("LuckPerms") != null) {
+                plugin.getLogger().info("✅ LuckPerms detected. LuckPerms sync hook successfully enabled.");
+                plugin.setEnabledLuckPermsHook(true);
+            } else {
+                plugin.getLogger().warning("⚠ LuckPerms not detected. Can't load LuckPerms sync hook.");
+            }
+        }
+        if (plugin.getConfig().getBoolean("hooks.minecraft_team.enabled", false)) {
+            plugin.getLogger().info("✅ Minecraft Team sync hook successfully enabled.");
+            plugin.setEnabledMinecraftTeamHook(true);
+        }
+    }
+}
---
+++ src/main/java/dev/drygo/XTeams/XTeams.java
@@ -0,0 +1,87 @@
+package dev.drygo.XTeams;
+
+import dev.drygo.XTeams.Hooks.LuckPerms.Managers.LuckPermsGroupManager;
+import dev.drygo.XTeams.Hooks.Minecraft.Managers.MinecraftTeamManager;
+import dev.drygo.XTeams.Utils.LoadUtils;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import dev.drygo.XTeams.Managers.ConfigManager;
+import dev.drygo.XTeams.Managers.TeamManager;
+import dev.drygo.XTeams.Utils.ChatUtils;
+import dev.drygo.XTeams.Utils.LogsUtils;
+
+import java.io.File;
+
+public class XTeams extends JavaPlugin {
+    public String version;
+    public String prefix;
+    public FileConfiguration messagesConfig;
+    private ChatUtils chatUtils;
+    public LogsUtils logsUtils;
+    public LoadUtils loadUtils;
+
+    public TeamManager teamManager;
+    public LuckPermsGroupManager luckPermsGroupManager;
+    public MinecraftTeamManager minecraftTeamManager;
+    public ConfigManager configManager;
+    private boolean workingPlaceholderAPI = false;
+    private boolean enabledLuckPermsHook = false;
+    private boolean enabledMinecraftTeamHook = false;
+
+    @Override
+    public void onEnable() {
+        this.version = getDescription().getVersion(); // Establecer versión dentro de onEnable
+        this.configManager = new ConfigManager(this);
+        this.messagesConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml"));
+        this.chatUtils = new ChatUtils(this, configManager);
+        this.logsUtils = new LogsUtils(this);
+        this.loadUtils = new LoadUtils(this, configManager);
+        this.teamManager = new TeamManager();
+        this.luckPermsGroupManager = new LuckPermsGroupManager(this);
+        this.minecraftTeamManager = new MinecraftTeamManager(this);
+        loadUtils.loadFeatures();
+        logsUtils.sendStartupMessage();
+    }
+
+    @Override
+    public void onDisable() {
+        configManager.saveTeamsToConfig();
+        logsUtils.sendShutdownMessage();
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+    public TeamManager getTeamManager() {
+        return teamManager;
+    }
+    public ChatUtils getChatUtils() {
+        return chatUtils;
+    }
+    public LuckPermsGroupManager getLuckPermsGroupManager() {
+        return luckPermsGroupManager;
+    }
+    public MinecraftTeamManager getMinecraftTeamManager() {
+        return minecraftTeamManager;
+    }
+
+    public boolean isWorkingPlaceholderAPI() {
+        return workingPlaceholderAPI;
+    }
+    public void setWorkingPlaceholderAPI(boolean workingPlaceholderAPI) {
+        this.workingPlaceholderAPI = workingPlaceholderAPI;
+    }
+    public boolean isEnabledLuckPermsHook() {
+        return enabledLuckPermsHook;
+    }
+    public void setEnabledLuckPermsHook(boolean enabledLuckPermsHook) {
+        this.enabledLuckPermsHook = enabledLuckPermsHook;
+    }
+    public boolean isEnabledMinecraftTeamHook() {
+        return enabledMinecraftTeamHook;
+    }
+    public void setEnabledMinecraftTeamHook(boolean enabledMinecraftTeamHook) {
+        this.enabledMinecraftTeamHook = enabledMinecraftTeamHook;
+    }
+}
